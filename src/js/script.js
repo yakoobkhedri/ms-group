55cc2b6cd1befa668030367318922d92b19de1bb
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-03-06 00:28:00 +0330

    commit message

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,65 @@
 
 // menu
 
-let btn = Array.from(document.querySelectorAll('.viza'));
-let tabContent = Array.from(document.querySelectorAll('.tabContent > div'));
-let dropdownBtn =Array.from(document.getElementsByClassName('dropdownBtn'));
-let dropdownBtn2 =Array.from(document.getElementsByClassName('dropdownBtn2'));
+// let btn = Array.from(document.querySelectorAll('.viza'));
+// let tabContent = Array.from(document.querySelectorAll('.tabContent > div'));
+// let dropdownBtn =Array.from(document.getElementsByClassName('dropdownBtn'));
+// let dropdownBtn2 =Array.from(document.getElementsByClassName('dropdownBtn2'));
 let hamIcon = document.getElementById('hamIcon');
 
 
-dropdownBtn.forEach((item)=>{
-  item.addEventListener('click', function () {
-    item.querySelector('.arrowSvg').classList.toggle('active');
-    item.nextElementSibling.classList.toggle('active');
-  })
-})
-dropdownBtn2.forEach((item)=>{
-  item.addEventListener('click', function () {
-    item.querySelector('.arrowSvg').classList.toggle('active');
-    item.nextElementSibling.nextElementSibling.classList.toggle('active');
-    item.nextElementSibling.classList.toggle('active');
-  })
+// dropdownBtn.forEach((item)=>{
+//   item.addEventListener('click', function () {
+//     item.querySelector('.arrowSvg').classList.toggle('active');
+//     item.nextElementSibling.classList.toggle('active');
+//   })
+// })
+// dropdownBtn2.forEach((item)=>{
+//   item.addEventListener('click', function () {
+//     item.querySelector('.arrowSvg').classList.toggle('active');
+//     item.nextElementSibling.nextElementSibling.classList.toggle('active');
+//     item.nextElementSibling.classList.toggle('active');
+//   })
+// })
+
+// btn.forEach((item) => {
+//   item.addEventListener('mouseover', function () {
+//     btn.forEach((items) => {items.classList.remove('active')});
+//     item.classList.add('active');
+//     let tabId = item.dataset.id;
+//     tabContent.forEach((content) => {
+//       let contentId = content.dataset.id;
+//       if (tabId === contentId) {
+//         content.classList.add('grid');
+//         content.classList.remove('hidden');
+//       } else {
+//         content.classList.remove('grid');
+//         content.classList.add('hidden');
+//       }
+//     })
+//   })
+// });
+
+hamIcon.addEventListener('click', function () {
+  hamIcon.classList.toggle('active');
+  document.querySelector('.mobileMenu').classList.toggle('active');
 })
 
-btn.forEach((item) => {
+// menu
+
+let list1 = Array.from(document.querySelectorAll('.list1 > a'));
+let list2 = Array.from(document.querySelectorAll('.list2 > ul'));
+let arrows = Array.from(document.querySelectorAll('.list1 img[alt="arrow"]'));
+let mobileMenu = document.querySelector('.mobileMenu');
+let serviceLink = document.getElementById('service-link');
+let dropdown = document.querySelector('.dropdown');
+
+list1.forEach((item) => {
   item.addEventListener('mouseover', function () {
-    btn.forEach((items) => {items.classList.remove('active')});
-    item.classList.add('active');
+    arrows.forEach((item)=>{item.classList.add('opacity-0')})
+    item.querySelector('img[alt="arrow"]').classList.remove('opacity-0');
     let tabId = item.dataset.id;
-    tabContent.forEach((content) => {
+    list2.forEach((content) => {
       let contentId = content.dataset.id;
       if (tabId === contentId) {
         content.classList.add('grid');
@@ -39,12 +71,19 @@ btn.forEach((item) => {
     })
   })
 });
-
-hamIcon.addEventListener('click', function () {
-  hamIcon.classList.toggle('active');
-  document.querySelector('.mobileMenu').classList.toggle('active');
+serviceLink.addEventListener('mouseenter', function () {
+  dropdown.classList.add('active');
+  document.querySelector('.servicesArrow').classList.add('opacity-100');
+  document.querySelector('.servicesArrow').classList.remove('opacity-0');
 })
 
+document.addEventListener('mousemove', (event) => {
+  if (!event.target.closest('.dropdown') && !event.target.closest('.dropdownArea')) {
+    dropdown.classList.remove('active');
+    document.querySelector('.servicesArrow').classList.remove('opacity-100');
+    document.querySelector('.servicesArrow').classList.add('opacity-0');
+  }
+})
 
 // swiper
 var banner = new Swiper(".banner", {
