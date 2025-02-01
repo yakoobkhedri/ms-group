3bbec8306f7bb58597b1558a3230a466c074a8ee
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-02-01 19:58:16 +0330

    commit message

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -11,3 +11,14 @@ closeSidebar.addEventListener('click' , function () {
   document.getElementById('sidebar').classList.remove('active');
   document.querySelector('.dashboardHeader').classList.add('active');
 })
+
+// checkbox
+
+let checkbox = Array.from(document.getElementsByClassName('checkbox'));
+let checkboxRow = Array.from(document.getElementsByClassName('checkboxRow'));
+
+checkboxRow.forEach((item) => {
+    item.addEventListener('click', function () {
+        item.querySelector('.checkbox').classList.toggle('active');
+    })
+})
